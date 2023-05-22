dc296e8856849cd35bc82b99a8aa34a701e7e3c7
sudirboyy <sudirboyy@gmail.com>
2023-05-22 16:02:07 +0700

    12

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -5,6 +5,16 @@ using namespace std;
 #include "ibu.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+    ibu* varIbu = new ibu("Dini");
+    ibu* varIbu2 = new ibu("Novi");
+    anak* varAnak1 = new anak("Tono");
+    anak* varAnak2 = new anak("Rini");
+    anak* varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
 }
 
