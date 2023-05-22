041216ef7214f2ae3b0782e557cd75bc971948ef
sudirboyy <sudirboyy@gmail.com>
2023-05-22 21:17:29 +0700

    fix

--- mainIA/anak.h
+++ mainIA/anak.h
@@ -9,7 +9,7 @@ public:
 		cout << "Anak \"" << nama << "\" ada \n";
 	}
 	~anak() {
-		cout << "Anak \"" << nama << "\" tidak ada \n"
+		cout << "Anak \"" << nama << "\" tidak ada \n";
 	}
 };
 #endif
