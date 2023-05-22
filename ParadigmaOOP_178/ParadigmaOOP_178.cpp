10aa79f197a38e3b3542ac44e62f64f52ab26345
sudirboyy <sudirboyy@gmail.com>
2023-05-22 15:27:04 +0700

    3

--- ParadigmaOOP_178/ParadigmaOOP_178.cpp
+++ ParadigmaOOP_178/ParadigmaOOP_178.cpp
@@ -9,9 +9,18 @@ private:
     float nilai;
 
 public:
-    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //member Ini
         //Definisi
+    }
+    ~mahasiswa() {
+        cout << "Id     = " << id << endl;
+        cout << "Nama   = " << nama << endl;
+        cout << "Nilai  = " << nilai << endl;
+
+    }
 };
+
+
 int main()
 {
     std::cout << "Hello World!\n";
