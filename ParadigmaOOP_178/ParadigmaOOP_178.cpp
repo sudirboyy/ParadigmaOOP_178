e18582a226ff07871739ea6eabe70b20f8090fe7
sudirboyy <sudirboyy@gmail.com>
2023-05-22 15:21:56 +0700

    2

--- ParadigmaOOP_178/ParadigmaOOP_178.cpp
+++ ParadigmaOOP_178/ParadigmaOOP_178.cpp
@@ -2,6 +2,16 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+        //Definisi
+};
 int main()
 {
     std::cout << "Hello World!\n";
