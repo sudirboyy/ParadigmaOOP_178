0a4eae6d01b5c3160be956020200ba5e6dc83d84
sudirboyy <sudirboyy@gmail.com>
2023-05-22 20:45:39 +0700

    17

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -2,6 +2,21 @@
 #include <vector>
 using namespace std;
 
+class dokter;
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
+	void tambahDokter(dokter*);
+	void cetakDokter();
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
